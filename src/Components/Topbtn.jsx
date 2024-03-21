45b78f3d6c53bc36f4bc783d3847cb34463ae565
SAV-7-X <148570005+SAV-7-X@users.noreply.github.com>
2024-03-21 11:09:33 +0530

    Update Topbtn.jsx

--- src/Components/Topbtn.jsx
+++ src/Components/Topbtn.jsx
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react';
+// import './BackToTopButton.css'; // Import CSS file for animations
 
 const BackToTopButton = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const [height, setHeight] = useState('bottom-4');
 
   useEffect(() => {
     const toggleVisibility = () => {
-      if (window.scrollY > 300) {
+      const scrolled = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const fullHeight = document.body.scrollHeight;
+
+      // Check if scrolled to the bottom of the page
+      if (scrolled + windowHeight >= fullHeight) {
         setIsVisible(true);
-      } else {
+        setHeight('top-60');
+        console.log("Reached the bottom of the page!");
+      } else if (scrolled === 0) {
         setIsVisible(false);
+        setHeight('bottom-4');
+      } else {
+        setIsVisible(true);
+        setHeight('bottom-4');
       }
     };
 
@@ -27,14 +40,16 @@ const BackToTopButton = () => {
   };
 
   return (
-    <button
-      className={`fixed bottom-4 font-semibold right-4 bg-gray-300 hover:bg-white text-black py-2 px-4 rounded z-20 flex ${isVisible ? 'fade-in' : 'fade-out'}`}
-      onClick={scrollToTop}
-    >
-      <span className="material-symbols-outlined">
-        arrow_upward
-      </span> Back to Top
-    </button>
+    <>
+      <button
+        className={`fixed ${height} font-semibold right-4 bg-gray-300 hover:bg-white text-black py-2 px-4 rounded z-20 flex ${
+          isVisible ? 'fade-in' : 'fade-out'
+        } ${height === 'top-60' && isVisible ? 'fade-ins' : ''}`}
+        onClick={scrollToTop}
+      >
+        <span className="material-symbols-outlined">arrow_upward</span> Back to Top
+      </button>
+    </>
   );
 };
 
